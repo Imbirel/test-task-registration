3ff2b86a81a19e030e1db1178c0fc1327d2993b6
Imbirel <katelin.kzm@gmail.com>
2026-04-08 05:37:40 +0300

    refactor(api): simplify UsersService

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,10 +1,5 @@
-import {
-  ConflictException,
-  Injectable,
-  Logger,
-  NotFoundException,
-} from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '#prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { RegistrationData } from '@packages/validation';
 
@@ -15,46 +10,26 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: RegistrationData) {
-    try {
-      const hashedPassword = await bcrypt.hash(data.password, 10);
-      return await this.prisma.user.create({
-        data: {
-          email: data.email,
-          name: data.name,
-          password: hashedPassword,
-        },
-        select: { id: true, email: true, name: true },
-      });
-    } catch (error: unknown) {
-      if (error && typeof error === 'object' && 'code' in error) {
-        if (error.code === 'P2002') {
-          throw new ConflictException(
-            'Пользователь с таким email уже зарегистрирован',
-          );
-        }
-      }
-      this.logger.error(error);
-      throw error;
-    }
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    return await this.prisma.user.create({
+      data: {
+        email: data.email,
+        name: data.name,
+        password: hashedPassword,
+      },
+      select: { id: true, email: true, name: true },
+    });
   }
 
   async findAll() {
     return this.prisma.user.findMany({
-      select: { id: true, email: true, name: true },
-      orderBy: { id: 'desc' },
+      select: { id: true, email: true, name: true, createdAt: true },
+      orderBy: { createdAt: 'desc' },
     });
   }
 
-  async remove(id: number) {
-    try {
-      return await this.prisma.user.delete({ where: { id } });
-    } catch (error: unknown) {
-      if (error && typeof error === 'object' && 'code' in error) {
-        if (error.code === 'P2025') {
-          throw new NotFoundException(`Пользователь с ID ${id} не найден`);
-        }
-      }
-      throw error;
-    }
+  async remove(id: string) {
+    this.logger.log(`Удаление пользователя с ID: ${id}`);
+    return await this.prisma.user.delete({ where: { id } });
   }
 }
